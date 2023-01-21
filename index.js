23e08ef8c4c0fde87e8ca40d4fca620f72e6fc89
alvert-git <albertbelbase018@gmail.com>
2023-01-21 13:31:10 +0545

    updated html,css and js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+let buttons = document.querySelectorAll('.button');
+let display = document.querySelector('.dis');
+buttons.forEach(buttons => {
+    buttons.addEventListener("click",()=>{
+        if(buttons.innerHTML == '='){
+            display.value=eval(display.value)
+        }
+        else if(buttons.innerHTML == 'AC'){
+            display.value = ''
+        }
+       else if(buttons.innerHTML == 'DEL'){
+            display.value = display.value.slice(0,-1)       }
+        else{
+        console.log(buttons.innerHTML)
+        display.value += buttons.innerHTML;}
+        
+        
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
