1ca8e08066527d9fc25e9631162d5f4579b0167b
alvert-git <albertbelbase018@gmail.com>
2023-01-22 20:39:49 +0545

    small change

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 let buttons = document.querySelectorAll('.button');
 let display = document.querySelector('.dis');
 buttons.forEach(buttons => {
-    buttons.addEventListener("click",()=>{
-        if(buttons.innerHTML == '='){
-            display.value=eval(display.value)
+    buttons.addEventListener("click", () => {
+        if (buttons.innerHTML == '=') {
+            display.value = eval(display.value)
         }
-        else if(buttons.innerHTML == 'AC'){
+        else if (buttons.innerHTML == 'AC') {
             display.value = ''
         }
-       else if(buttons.innerHTML == 'DEL'){
-            display.value = display.value.slice(0,-1)       }
-        else{
-        console.log(buttons.innerHTML)
-        display.value += buttons.innerHTML;}
-        
-        
+        else if (buttons.innerHTML == 'DEL') {
+            display.value = display.value.slice(0, -1)
+        }
+        else {
+            console.log(buttons.innerHTML)
+            display.value += buttons.innerHTML;
+        }
+
+
     })
 });
 
